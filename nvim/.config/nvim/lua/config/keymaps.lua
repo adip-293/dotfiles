45e78c0792751ebae7502697b764e2a2da8ca2b5
adip-293 <adityapola4@gmail.com>
2025-06-18 22:51:07 -0400

    Tranferring to Snacks

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,8 @@
-vim.g.mapleader = " "
+ vim.g.mapleader=" "
 
 -- Telescope keymaps
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
-
-map('n', '<leader>ff', "<cmd>Telescope find_files<cr>", opts)
-map('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", opts)
-map('n', '<leader>fb', "<cmd>Telescope buffers<cr>", opts)
-map('n', '<leader>fh', "<cmd>Telescope help_tags<cr>", opts)
-
--- Neo-tree keymap
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = "Toggle Neo-tree" })
 
 -- Oil file manager keymap
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory (oil.nvim)", noremap = true, silent = true })
@@ -20,5 +12,23 @@ vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previou
 vim.keymap.set("n", "<leader>bd", "<Cmd>bdelete<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>bl", "<Cmd>BufferLineCloseRight<CR>", { desc = "Close buffers to right" })
 vim.keymap.set("n", "<leader>bh", "<Cmd>BufferLineCloseLeft<CR>", { desc = "Close buffers to left" })
+
+vim.keymap.set("n", "<leader>e",   function() Snacks.explorer() end, { desc = "Open Snacks Explorer" })
+vim.keymap.set("n", "<leader>ff",  function() Snacks.picker.files() end, { desc = "Open Snacks File Picker" })
+vim.keymap.set("n", "<leader>fb",  function() Snacks.picker.buffers() end, { desc = "Open Snacks Buffer Picker" })
+vim.keymap.set("n", "<leader>fp",  function() Snacks.picker.projects() end, { desc = "Open Snacks Project Picker" })
+vim.keymap.set("n", "<leader>fr",  function() Snacks.picker.recent() end, { desc = "Open Snacks Recent Picker" })
+vim.keymap.set("n", "<leader>fg",  function() Snacks.picker.grep() end, { desc = "Open Grep" })
+vim.keymap.set("n", "<leader>fo",  function() Snacks.picker.grep_buffers() end, { desc = "Open Grep" })
+vim.keymap.set("n", "<leader>fw",  function() Snacks.picker.grep_word() end, { desc = "Open Grep" })
+vim.keymap.set("n", "<leader>gb",  function() Snacks.gitbrowse() end, { desc = "Open Grep" })
+vim.keymap.set("n", "<leader>gt",  function() Snacks.lazygit() end, { desc = "Open Grep" })
+
+
+
+
+
+
+
 
 
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,37 @@
+return {
+  "folke/snacks.nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  priority = 1000,
+  lazy = false,
+   opts = {
+    picker = {enabled = true},
+    bigfile = {enabled =true},
+    dim = {enabled = true},
+    explorer = {enabled = true},
+    indent = {enabled = true},
+    scroll = {enabled = true},
+    gitbrowse = {enabled = true},
+    lazygit = {enabled = true},
+    notifier = {enabled = true},
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+        ]],
+      },
+    }, 
+  },
+  
+
+}
+
